06225a08b6ea615bc9d1fcd957730f971016e694
Thanh Tan Le <thanhtan1105@gmail.com>
2024-05-14 23:35:26 +0700

    refactor lecture 02

--- app/flink_jobs/kafka_to_postgres_data_pipeline_lecture02.py
+++ app/flink_jobs/kafka_to_postgres_data_pipeline_lecture02.py
@@ -7,16 +7,6 @@ from pyflink.common import Types, Row
 from pyflink.common.typeinfo import RowTypeInfo
 from app.model.stock import Stock
 from app.functions.flink_function import PostgresUpsertSinkFunction
-import logging
-
-# Set up logging
-logging.basicConfig(level=logging.DEBUG)
-logging.info("Script started")
-
-topic = "stock"
-bootstrap_servers = KAFKA_CONFIG['bootstrap_servers']
-group_id = KAFKA_CONFIG['group_id']
-scan_startup_mode = KAFKA_CONFIG['scan_startup_mode']
 
 # get the StreamExecutionEnvironment
 env = StreamExecutionEnvironment.get_execution_environment()
@@ -31,8 +21,7 @@ env = StreamExecutionEnvironment.get_execution_environment()
  .set_checkpoint_storage(FileSystemCheckpointStorage(FLINK_CONFIGURE['checkpoint']['storage_lecture02']))
 )
 
-settings = EnvironmentSettings.new_instance().in_streaming_mode().build()
-t_env = StreamTableEnvironment.create(env, environment_settings=settings)
+t_env = StreamTableEnvironment.create(env, environment_settings=EnvironmentSettings.new_instance().in_streaming_mode().build())
 
 (t_env.get_config()
  .get_configuration()
@@ -50,18 +39,22 @@ CREATE TABLE KafkaSource (
     'scan.startup.mode' = '{}',
     'format' = 'json'
 )
-""".format(Stock.asflink_structure(), topic, bootstrap_servers, group_id, scan_startup_mode)
+""".format(Stock.asflink_structure(), 'stock', KAFKA_CONFIG['bootstrap_servers'], KAFKA_CONFIG['group_id'], KAFKA_CONFIG['scan_startup_mode'])
 
 t_env.execute_sql(kafka_source)
 
-kafka_source_table = t_env.sql_query("SELECT ticker, price, volume, open_price, high, low, close_price, adjusted_close FROM KafkaSource")
 kafka_source_stream = t_env.to_append_stream(
-    kafka_source_table,
+    t_env.sql_query("SELECT ticker, price, volume, open_price, high, low, close_price, adjusted_close FROM KafkaSource"),
     RowTypeInfo(
         [Types.STRING(), Types.DOUBLE(), Types.INT(), Types.DOUBLE(), Types.DOUBLE(), Types.DOUBLE(), Types.DOUBLE(), Types.DOUBLE()],
         ["ticker", "price", "volume", "open_price", "high", "low", "close_price", "adjusted_close"]
     )
 )
+
+kafka_source_stream = kafka_source_stream.map(lambda row: PostgresUpsertSinkFunction.transform_row(row), RowTypeInfo(
+        [Types.STRING(), Types.DOUBLE(), Types.INT(), Types.DOUBLE(), Types.DOUBLE(), Types.DOUBLE(), Types.DOUBLE(), Types.DOUBLE()],
+        ["ticker", "price", "volume", "open_price", "high", "low", "close_price", "adjusted_close"]
+    ))
 
 kafka_source_stream.add_sink(PostgresUpsertSinkFunction(DATABASE_CONFIG['url'], DATABASE_CONFIG['username'], DATABASE_CONFIG['password']).invoke())
 env.execute("Kafka to Postgres Data Pipeline Lecture 02")
